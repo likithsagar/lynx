df561e25428021f509efeb94241b494679c13d03
likithsagar <likithsagar9@gmail.com>
2018-04-02 13:42:29 +0530

    add remaining pages

---
+++ app/src/main/java/com/example/likit/lynx/TestingReminderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.likit.lynx
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestingReminderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing_reminder)
+    }
+}
